d15f245e2ae2ead51937c3edfb055367a711c99b
ArsenIbragim01 <arsen1br@icloud.com>
2025-07-25 15:58:06 +0500

    this update more reader then before

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
+from typing import List
+
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        hashmap = {}
-        for index in range(len(nums)):
-            number = nums[index]
-            complement = target - number
-            if complement in hashmap:
-                return index, hashmap[complement]
-            else:
-                hashmap[number] = index
-        return 'Not valid'
+    """
+    Класс, содержащий решение задачи twoSum.
+    """
+    def two_sum(self, nums: List[int], target: int) -> List[int]:
+        """
+        Находит два числа в списке, которые в сумме дают определенное целевое значение.
+
+        Аргументы:
+            nums: Список целых чисел.
+            target: Целевое целое число.
+
+        Возвращает:
+            Список, содержащий индексы двух чисел.
+
+        Исключения:
+            ValueError: Если никакие два числа не суммируются до целевого значения.
+        """
+        num_to_index = {}
+        for index, num in enumerate(nums):
+            complement = target - num
+            if complement in num_to_index:
+                return [num_to_index[complement], index]
+            num_to_index[num] = index
+        raise ValueError("Нет решения для two sum")
