ce428a49fe6df5143f3819c91028f8afe7408799
ArsenIbragim01 <arsen1br@icloud.com>
2025-07-25 15:52:44 +0500

    ewvewve

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Python
 
 its just a testing my skill in github
+
+## i added new class two_sum
+
--- main.py
+++ main.py
@@ -1,14 +1,11 @@
-import cmath
-
-a = 1
-b = 5
-c = 6
-
-# calculate the discriminant
-d = (b**2) - (4*a*c)
-
-# find two solutions
-sol1 = (-b-cmath.sqrt(d))/(2*a)
-sol2 = (-b+cmath.sqrt(d))/(2*a)
-
-print('The solution are {0} and {1}'.format(sol1,sol2))
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashmap = {}
+        for index in range(len(nums)):
+            number = nums[index]
+            complement = target - number
+            if complement in hashmap:
+                return index, hashmap[complement]
+            else:
+                hashmap[number] = index
+        return 'Not valid'
